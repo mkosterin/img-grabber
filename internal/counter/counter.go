70926f3fd982ff8c52ede046fea603e4ecc41aee
Maksim Kosterin <maksim_kosterin@epam.com>
2023-04-17 17:01:37 +0200

    Remove old code

--- internal/counter/counter.go
+++ internal/counter/counter.go
@@ -18,12 +18,6 @@ func Parse(str string) (error, string, bool) {
 	if matched {
 		step1 := strings.ReplaceAll(strings.TrimSpace(str), "image: ", "")
 		step2 := strings.ReplaceAll(strings.TrimSpace(step1), "\"", "")
-		//step3 := strings.Split(step2, ":")
-		//if len(step3) == 2 {
-		//	return nil, step3[0], step3[1]
-		//} else {
-		//	return nil, step3[0], "latest"
-		//}
 		return nil, step2, false
 	}
 	return errors.New("not image string"), "", false
